f44ca7e31a64562668e15f0cfb9f9afee0a69b49
a <a@a.a>
2020-06-10 11:57:48 +0200

    test turn server

--- bugout-signal-test.js
+++ bugout-signal-test.js
@@ -1267,10 +1267,17 @@
             {
                 urls: [
                     'stun:stun.l.google.com:19302',
-                    'stun:global.stun.twilio.com:3478',
-                    'stun:stun.avigora.fr:3478',
+//                    'stun:global.stun.twilio.com:3478',
+//                    'stun:stun.avigora.fr:3478',
                     'stun:stun.1und1.de:3478'
                 ]
+            },
+            {
+                "urls": [
+                    "turn:numb.viagenie.ca"
+                ],
+                "username":"webrtc@live.com",
+                "credential":"muazkh"
             }
         ];
         window.nkt.userList = {};
